44ef26cd08d8c72ff3d10c51bc80520301391284
Justin Nuß <nuss.justin@gmail.com>
2023-01-27 23:43:58 +0100

    Move example types and globals to different file

--- feature_test.go
+++ feature_test.go
@@ -27,32 +27,6 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 )
 
-// For use in examples
-type Post struct {
-}
-
-// For use in examples
-var myCtx = context.Background()
-
-// For use in examples
-var postId int
-
-// For use in examples
-func loadPost(context.Context, int) (Post, error)          { return Post{}, nil }
-func loadPostOptimized(context.Context, int) (Post, error) { return Post{}, nil }
-
-// For use in examples
-var testerStrategy feature.Strategy
-
-// For use in examples
-func readFlags(string) map[string]feature.Strategy {
-	return nil
-}
-
-func IsTester(context.Context) bool {
-	return false
-}
-
 func ExampleIf() {
 	testerStrategy = feature.StrategyFunc(func(ctx context.Context, _ string) feature.Decision {
 		// Enable all flags for testers
